f61d32060e5e3ef11dccc09e9edf75b725472cf6
DynamicApproach <tlloyd-jones@albany.edu>
2022-06-27 09:25:35 -0400

    Add Prettier, ESLint, ignore files, +tailw compat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import type { ReactElement } from "react";
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
