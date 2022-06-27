efb08634d517c882997e1d2457f9d1a0c3788a46
DynamicApproach <tlloyd-jones@albany.edu>
2022-06-27 10:39:53 -0400

    Add index, projects and nav

---
+++ pages/Nav.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+export const Nav = () => {
+  return (
+    <div className="border-gray-200 relative  top-0  right-0 flex  min-w-0 justify-between bg-darkgray px-6 ">
+      <div className="top-0 right-0 m-5 flex bg-darkgray">
+        <nav className="flex flex-wrap items-center justify-between overflow-x-auto bg-darkgray p-2 text-blue ">
+          <div className="inline-block w-auto flex-wrap lg:flex lg:w-auto lg:items-center">
+            <div className="text-sm lg:flex-grow">
+              <Link href="/Projects" target="_blank">
+                <p className="mt-4 mr-4 block cursor-pointer font-sans text-nextblue hover:text-white lg:mt-0">
+                  Projects
+                </p>
+              </Link>{" "}
+              <Link href="/printing">
+                <p className="mt-4 mr-4 block cursor-pointer font-sans text-nextblue hover:text-white lg:mt-0">
+                  3D Printing
+                </p>
+              </Link>
+              <Link href="/print">
+                <p className="mt-4 mr-4 block cursor-pointer font-sans text-nextblue hover:text-white lg:mt-0">
+                  ThreeJS
+                </p>
+              </Link>
+              <Link
+                href="https://github.com/DynamicApproach/"
+                target={"_blank"}
+              >
+                <p className="mt-4 mr-4 block cursor-pointer font-sans text-nextblue hover:text-white lg:mt-0">
+                  GitHub
+                </p>
+              </Link>
+            </div>
+            <Link href="/">
+              <p className="mt-4 mr-4 block cursor-pointer font-sans text-nextblue hover:text-white lg:mt-0">
+                Home
+              </p>
+            </Link>
+            <div className="cursor-pointer space-x-1 pl-4">
+              <Link href="mailto: tlloyd-jones@albany.edu">
+                <p className=" hover:border-transparent hover:text-black mt-4 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:bg-nextblue lg:mt-0">
+                  Email Me &rarr;
+                </p>
+              </Link>
+              <a>
+                <p className=" hover:border-transparent hover:text-black mt-4 inline-block rounded border border-white px-4 py-2 pl-4 text-sm leading-none text-white hover:bg-nextblue lg:mt-0">
+                  Resume &rarr;
+                </p>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+export default Nav;
---
+++ pages/Projects.tsx
@@ -0,0 +1,55 @@
+import Nav from "pages/Nav";
+import styles from "styles/Projects.module.css";
+export const projects = () => {
+  return (
+    <div className="bg-darkgray">
+      <Nav />
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Recent Projects</h1>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Discord Pager &rarr;</h2>
+            <p>
+              Program that listens for specific messages with a leading tag via
+              Discord API and relayed those messages by serial to a scrolling
+              LCD attached to an Arduino Nano.
+            </p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>MIPS Calc &rarr;</h2>
+
+            <p>
+              Created a calculator using a binary tree in MIPS Assembly to find
+              the result, prefix, and the postfix of a parenthesized expression
+            </p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Project3 &rarr;</h2>
+            <p>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Project4&rarr;</h2>
+            <p>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default projects;
