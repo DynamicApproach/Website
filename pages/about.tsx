a6507122701d956f5176316d393819e095f9a998
DynamicApproach <tlloyd-jones@albany.edu>
2022-06-28 01:15:04 -0400

    About generic page added

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Nav from "./Nav";
+export const about = () => {
+  return (
+    <div className="bg-darkgray">
+      <Nav />
+      <h1 className="min-h-50% text-center">about</h1>
+    </div>
+  );
+};
+export default about;
