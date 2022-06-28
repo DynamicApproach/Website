f51ccda766266dbd62439ebbc23aa6b925dbea30
DynamicApproach <tlloyd-jones@albany.edu>
2022-06-28 06:25:41 -0400

    nav fix

--- pages/Nav.tsx
+++ pages/Nav.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 import styled from "styled-components";
 import tw from "tailwind-styled-components";
-const TailWindNav = tw.nav`flex justify-center sticky top-0 float-right m-2 bg-backgray p-6"`;
-const TailwindButton = tw.button`border-indigo-500  m-15 cursor-pointer  border-spacing-80 rounded-full
-border-2 text-center align-middle text-nextblue hover:border-nextblue hover:text-white`;
-const TailWindMenuBlock = tw.div`content-center justify-center align-middle text-sm `;
-const TailWindMenu = tw.div`inline-block min-w-full  flex-wrap items-center bg-backgray`;
-const TailWindMenuBG = tw.div`sticky border-spacing-80`;
+const TailWindNav = tw.nav` justify-center sticky top-0 min-w-max float-right    bg-backgray p-6"`;
+const TailwindButton = tw.button`border-indigo-500  cursor-pointer  rounded-full px-0.5 text-2xl
+border-2 text-center border-spacing-96 align-middle text-nextblue hover:border-nextblue hover:text-white`;
+const TailWindMenuBlock = tw.div`content-center flow-root border-spacing-6   space-y-6 
+justify-center align-middle text-sm `;
+const TailWindMenu = tw.div` bg-backgray flex-row justify-center align-middle`;
+const TailWindMenuBG = tw.div`relative min-w-full`;
 const TailwindExitButton = tw.div`hover:border-transparent hover:text-black m-2 mt-4
-inline-block rounded  border  border-white px-4 py-2 text-sm leading-none
+inline-block  rounded-tl-lg rounded-bl-lg  border space-y-7  border-white px-4 py-2 text-sm leading-none
  text-white hover:bg-nextblue lg:mt-0`;
 
 export const Nav = () => {
@@ -22,8 +23,10 @@ export const Nav = () => {
             <Link href="/Projects">
               <TailwindButton>Projects</TailwindButton>
             </Link>
+          </TailWindMenuBlock>
+          <TailWindMenuBlock>
             <Link href="/printing">
-              <TailwindButton>Projects</TailwindButton>
+              <TailwindButton>3D Printing</TailwindButton>
             </Link>
           </TailWindMenuBlock>
           <TailWindMenuBlock>
