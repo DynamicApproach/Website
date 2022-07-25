466bf8dc6bafcd264db64f6eeb07efe8896fec05
DynamicApproach <tlloyd-jones@albany.edu>
2022-07-25 01:00:57 -0400

    Refactor document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,35 +7,14 @@ import Document, {
   DocumentContext
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import withServerStyleSheet from "../utils/withServerStyleSheet";
 // https://nextjs.org/docs/advanced-features/custom-document -- TypeScript example + documentation
 
-const withStyledComponents = async (
-  ctx: DocumentContext
-): Promise<DocumentInitialProps> => {
-  const { renderPage } = ctx;
-  const sheet = new ServerStyleSheet();
-
-  try {
-    ctx.renderPage = () =>
-      renderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-      });
-
-    const { styles, ...initialProps } = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [styles, sheet.getStyleElement()]
-    };
-  } finally {
-    sheet.seal();
-  }
-};
 export default class MyDocument extends Document {
   public static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    return withStyledComponents(ctx);
+    return withServerStyleSheet(ctx);
   }
 
   render() {
---
+++ utils/withServerStyleSheet.tsx
@@ -0,0 +1,27 @@
+import { DocumentContext, DocumentInitialProps } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+const withServerStyleSheet = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps> => {
+  const { renderPage } = ctx;
+  const sheet = new ServerStyleSheet();
+
+  try {
+    ctx.renderPage = () =>
+      renderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+      });
+
+    const { styles, ...initialProps } = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [styles, sheet.getStyleElement()]
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+export default withServerStyleSheet;
