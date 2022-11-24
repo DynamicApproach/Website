84e1d26ba18720440107d9410d20aceb6de10932
DynamicApproach <tlloyd-jones@albany.edu>
2022-11-24 18:46:25 -0500

    Index and Nav fix

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 //import Image from "next/image";
+import { useState } from "react";
 
+import mainimg from "pages/images/main.png";
 import tw from "tailwind-styled-components";
 const TailWindNav = tw.nav` justify-center sticky top-0 min-w-max float-right    bg-transparent p-6`;
 const TailWindMenuBlock = tw.div`content-center flow-root border-spacing-6   space-y-6 
@@ -10,43 +12,72 @@ inline-block  rounded-tl-lg rounded-bl-lg  border space-y-7  border-white px-4 p
  text-white hover:bg-nextblue lg:mt-0`;
 
 export const Nav = () => {
-  return (
-    <TailWindNav>
-      <TailWindMenuBlock>
-        <Link href="/" target={"_blank"}>
-          <TailwindExitButton>Home</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
-      <TailWindMenuBlock>
-        <Link href="/infoPages/Projects" target={"_blank"}>
-          <TailwindExitButton>Projects</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
-      <TailWindMenuBlock>
-        <Link href="/infoPages/threedprinting" target={"_blank"}>
-          <TailwindExitButton>3D Printing</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
-      <TailWindMenuBlock>
-        <Link href="/infoPages/print" target={"_blank"}>
-          <TailwindExitButton>Threejs</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
-      <TailWindMenuBlock>
-        <Link href="https://github.com/DynamicApproach/" target={"_blank"}>
-          <TailwindExitButton>GitHub</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
+  const [active, setActive] = useState(false);
 
-      <TailWindMenuBlock>
-        <Link href="mailto: tlloyd-jones@albany.edu" target={"_blank"}>
-          <TailwindExitButton>Email Me &rarr;</TailwindExitButton>
-        </Link>
-      </TailWindMenuBlock>
-      <TailWindMenuBlock>
-        <TailwindExitButton>Resume &rarr;</TailwindExitButton>
-      </TailWindMenuBlock>
-    </TailWindNav>
+  const clickOnNav = () => {
+    setActive(!active);
+  };
+  return (
+    <nav className="bg-green-400 flex flex-wrap items-center p-3">
+      <a href="https://github.com/DynamicApproach">
+        <img
+          src="https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&width=435&lines=DynamicApproach"
+          alt="Typing SVG"
+        />
+      </a>
+      <button
+        className=" hover:bg-green-600 ml-auto inline-flex rounded p-3
+         text-white outline-none hover:text-white "
+        onClick={clickOnNav}
+      >
+        <svg
+          className="h-6 w-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+      <div
+        className={`${
+          active ? "" : "hidden"
+        }   w-full lg:inline-flex lg:w-auto lg:flex-grow`}
+      >
+        <div
+          className="flex w-full flex-col items-start 
+        lg:ml-auto lg:inline-flex  lg:h-auto lg:w-auto lg:flex-row lg:items-center"
+        >
+          <TailWindMenuBlock>
+            <Link href="/" target={"_blank"}>
+              <TailwindExitButton>Home</TailwindExitButton>
+            </Link>
+            <Link href="/infoPages/Projects" target={"_blank"}>
+              <TailwindExitButton>Projects</TailwindExitButton>
+            </Link>
+            <Link href="/infoPages/threedprinting" target={"_blank"}>
+              <TailwindExitButton>3D Printing</TailwindExitButton>
+            </Link>
+            <Link href="/infoPages/print" target={"_blank"}>
+              <TailwindExitButton>Threejs</TailwindExitButton>
+            </Link>
+            <Link href="https://github.com/DynamicApproach/" target={"_blank"}>
+              <TailwindExitButton>GitHub</TailwindExitButton>
+            </Link>
+            <Link href="mailto: tlloyd-jones@albany.edu" target={"_blank"}>
+              <TailwindExitButton>Email Me &rarr;</TailwindExitButton>
+            </Link>
+            <TailwindExitButton>Resume &rarr;</TailwindExitButton>
+          </TailWindMenuBlock>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Nav from "pages/components/Nav";
 import arrowr from "pages/images/arrowr.png";
 import { Lightmode } from "pages/components/Lightmode";
 
+import mainimg from "pages/images/main.png";
 const addonList = [
   "NPM, ",
   "GIT, ",
@@ -22,15 +23,15 @@ const schoolprojects = [
   "Computer Security, Databases, Algorithms, and Programming Languages"
 ];
 const internship = [
-  "Data anylasis, ",
+  "Data analysis, ",
   "and data visualization ",
   " for a local company to gather sales data and visualize it in a way that is easy to understand. "
 ];
 
 const Index: NextPage = () => (
   <div
-    className="light:bg-white h-screen max-h-full bg-backgray bg-gradient-to-r
-   from-backgray to-albanypurp bg-local "
+    className="light:bg-white  from-slate-200 to-slate-700 min-h-screen bg-backgray
+   bg-gradient-to-r bg-cover bg-scroll "
   >
     <Nav />
     <Head>
@@ -38,19 +39,37 @@ const Index: NextPage = () => (
       <link rel="icon" href="/images/melongray.png" className="border" />
     </Head>
     <main className="bg-transparent light:text-black">
-      <div className="gap-4 pl-20 pb-3 align-middle">
-        <h2
-          className="col-span-3 items-center justify-center gap-4 
-         pr-4 text-center align-middle text-2xl text-albanyyellow"
-        >
+      <div className="grid grid-cols-4 ">
+        <div className="flex justify-end pr-2">
+          <Image
+            src={mainimg}
+            alt="Thomas Lloyd-Jones"
+            width={75}
+            height={75}
+            className="rounded-full"
+          />
+        </div>
+        <h2 className=" col-span-3 content-center text-2xl text-albanyyellow">
           Thomas Lloyd-Jones
+          {/* Add circulr image  */}
+          <p className="text-sm">Welcome to my personal website.</p>
         </h2>
       </div>
-
-      <div className="grid h-auto w-auto grid-cols-2 border-solid border-albanylightpurp pl-5">
-        <div className="">
-          <p className={styles.descriptiondoubleb}>
-            Currently, I&apos;m working on: <br></br> <br></br>
+      <div className="grid  w-auto grid-cols-4 border-solid border-albanylightpurp pl-5 pt-8">
+        <div className=" ">
+          <p className=" pr-4 text-nextlightblueish">
+            My favourite things are coding, networking and playing video games.
+            While currently studying Computer Science at SUNY at Albany, I am
+            also working on my skills, website and side projects.
+          </p>
+          <br></br>
+          <p className=" pr-4 text-nextlightblueish">
+            This is one of a few side projects currently under construction.
+          </p>
+        </div>
+        <div className=" ">
+          <p className=" pr-4 text-nextlightblueish">
+            Currently, I&apos;m working on: <br></br>
             {internship}
             <br></br>
             <br></br>
@@ -58,39 +77,27 @@ const Index: NextPage = () => (
             {schoolprojects}
           </p>
         </div>
-        <div className="">
-          <p className={styles.descriptiondoubleb}>
-            I am almost finished with my Bachelors of Science in Computer
-            Science, with a planned graduation date of December 2023.
-            <br></br>
-            <br></br>
-            In my spare time, I tend to create new things, and I am currently
-            working on a new project, and this website. <br></br>
-          </p>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 ">
-        <p></p>
-        <div className=" align-between pb-3 ">
-          <p className=" text-nextlightblueish">
-            My favourite things are coding, networking and playing video games.
-            While currently studying as a senior at SUNY at Albany, I am also
-            working on my portfolio. I am a software engineer with a passion for
-            learning and building things.
-          </p>
-        </div>
+        <p className=" pr-4 text-nextlightblueish">
+          I am almost finished with my Bachelors of Science in Computer Science,
+          with a planned graduation date of December 2023.
+          <br></br>
+          <br></br>
+          In my spare time, I tend to create new things, and I am currently
+          working on a new project, and this website. <br></br>
+        </p>
       </div>
     </main>
+    <br></br>
     <div
-      className="bg-transparent  relative grid 
-    grid-cols-3 content-center justify-center border-t-4 border-albanylightpurp p-8 "
+      className="bg-transparent  relative  grid grid-cols-3 
+    content-center justify-center border-t-4 border-albanylightpurp p-8 "
     >
       <div></div>
       <footer className=" ">
-        <div className=" max-w-m bg-transparent flex min-w-min flex-wrap content-center justify-center ">
+        <div className=" max-w-m bg-transparent flex min-w-min  flex-wrap content-center justify-center  ">
           <div
-            className="color-gray left-0 m-4 min-w-min max-w-xs border-spacing-4 
-        border border-solid border-albanylightpurp
+            className="color-gray left-0 m-4 min-w-min max-w-xs  border-spacing-4 border 
+        border-solid border-albanylightpurp 
          p-6 text-left text-albanyyellow"
           >
             <h3 className="underline">Powered by</h3>{" "}
