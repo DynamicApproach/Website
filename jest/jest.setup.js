8e10d921ca5466c64b439f411360b7245af390bd
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2022-11-28 01:13:52 -0500

    fix jest fail from cursor fuct

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,1 +1,30 @@
 import "@testing-library/jest-dom/extend-expect";
+beforeAll(() => {
+
+    Object.defineProperty(window, "matchMedia", {
+
+        writable: true,
+
+        value: jest.fn().mockImplementation(query => ({
+
+            matches: false,
+
+            media: query,
+
+            onchange: null,
+
+            addListener: jest.fn(), // Deprecated
+
+            removeListener: jest.fn(), // Deprecated
+
+            addEventListener: jest.fn(),
+
+            removeEventListener: jest.fn(),
+
+            dispatchEvent: jest.fn(),
+
+        }))
+
+    });
+
+});
