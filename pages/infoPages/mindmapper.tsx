6f431e176b3e5bfbb10173bf520dbd2001a72901
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2023-05-10 21:17:51 -0400

    Fix upload / download

--- pages/infoPages/mindmapper.tsx
+++ pages/infoPages/mindmapper.tsx
@@ -9,8 +9,9 @@ const MindMapper = () => {
   const [requestKey, setRequestKey] = useState(0);
 
   const handleOpenAIResponse = (response: any) => {
-    // Check if the response is an object and not a JSON string
-
+    if (typeof response === "object") {
+      response = JSON.stringify(response);
+    }
     setOpenAIResponse(response);
     setHasResponse(true);
     setRequestKey((requestKey) => requestKey + 1);
@@ -23,20 +24,18 @@ const MindMapper = () => {
         <div className="h-full w-full max-w-md flex-1 p-4">
           <h1 className="mb-4 text-center text-2xl font-semibold">
             Welcome To The Mindmap Creator.
-          </h1>
+          </h1>{" "}
           <h1 className="text-l mb-4 text-center font-semibold">
             {" "}
             Enter a subject you would like a map of in the box below. It can
             take a second to load!
           </h1>
-
           <h2 className="sr-only">A mind map of:</h2>
           <OpenAIInput onResponse={handleOpenAIResponse} />
         </div>
         {hasResponse && (
           <div className="flex-2 h-full w-full">
             <MarkmapOutput content={openAIResponse} key={requestKey} />{" "}
-            {/* Add the key prop here */}
           </div>
         )}
       </div>
