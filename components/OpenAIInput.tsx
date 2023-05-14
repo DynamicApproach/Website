c665f43a3c8ac00c2160c03f5c9a643a6dc05633
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2023-05-14 06:28:38 -0400

    Fix up format for md insertion

--- components/OpenAIInput.tsx
+++ components/OpenAIInput.tsx
@@ -141,6 +141,8 @@ function modifyMarkdown(
   // Convert all - to * (for markdown lists)
   newData = newData.replace(/-/g, "*");
 
+  // Format new data
+  newData = formatData(newData);
   console.log("New data:", newData);
   const lines = markdown.split("\n");
   const nodeLineIndex = lines.findIndex((line) => line.includes(node));
@@ -193,6 +195,30 @@ function modifyMarkdown(
   lines.splice(insertIndex, 0, ...newNodeData);
 
   return lines.join("\n");
+}
+
+function formatData(data: string): string {
+  let currentIndentationLevel = 0;
+
+  return data
+    .split("\n")
+    .map((line) => {
+      // Count the number of asterisks at the beginning of the line
+      const match = line.match(/^\**/);
+      const numAsterisks = match ? match[0].length : 0;
+
+      if (numAsterisks > currentIndentationLevel) {
+        // If this line is more indented than the previous line, increase the indentation level
+        currentIndentationLevel++;
+      } else if (numAsterisks < currentIndentationLevel) {
+        // If this line is less indented than the previous line, decrease the indentation level
+        currentIndentationLevel--;
+      }
+
+      // Add two spaces of indentation for each indentation level
+      return "  ".repeat(currentIndentationLevel) + line.trim();
+    })
+    .join("\n");
 }
 
 function getNodePath(markdown: string, node: string): string {
