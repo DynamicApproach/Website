344765e78fdf5fde5901c851f686e7b0cc0ac1a0
DynamicApproach <lloydjonesthomas@gmail.com>
2023-06-28 00:34:56 -0400

    Update basic privacy

---
+++ pages/privacy.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div
+      className="min-w-screen bg-grad  min-h-screen bg-gradient-to-br
+    from-backgray to-albanypurp bg-cover bg-scroll text-white"
+    >
+      <h1>Privacy Policy</h1>
+      <p>
+        We take your privacy seriously. This privacy policy describes what
+        personal information we collect and how we use it.
+      </p>
+      <h2>Routine Information Collection</h2>
+      <p>
+        All web servers track basic information about their visitors. This
+        information includes, but is not limited to, IP addresses, browser
+        details, timestamps and referring pages. None of this information can
+        personally identify specific visitors to this site. The information is
+        tracked for routine administration and maintenance purposes.
+      </p>
+      <h2>Cookies and Web Beacons</h2>
+      <p>
+        Where necessary, we use cookies to store information about a
+        visitor&apos;s preferences and history in order to better serve the
+        visitor and/or present the visitor with customized content.
+      </p>
+      <h2>Controlling Your Privacy</h2>
+      <p>
+        Note that you can change your browser settings to disable cookies if you
+        have privacy concerns. Disabling cookies for all sites is not
+        recommended as it may interfere with your use of some sites. The best
+        option is to disable or enable cookies on a per-site basis. Consult your
+        browser documentation for instructions on how to block cookies and other
+        tracking mechanisms.
+      </p>
+      <h2>Contact Information</h2>
+      <p>
+        Concerns or questions about this privacy policy can be directed to our
+        contact page for further clarification.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
