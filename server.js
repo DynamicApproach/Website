3fd8c7898191b33689097b7ca60596e67879a26c
DynamicApproach <lloydjonesthomas@gmail.com>
2023-07-02 13:46:08 -0400

    Port for digi

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "0.0.0.0";
-const port = 3000;
+const port = 8080;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
